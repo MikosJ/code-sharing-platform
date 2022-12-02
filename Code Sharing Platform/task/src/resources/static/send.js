e38a6959ddc0a038a6d61a848482deee8bea61e4
MikosJ <jakubdmikos@gmail.com>
2022-12-02 19:49:57 +0100

    styled endpoints

--- Code Sharing Platform/task/src/resources/static/send.js
+++ Code Sharing Platform/task/src/resources/static/send.js
@@ -1,4 +1,6 @@
 function send() {
+    console.log(document.getElementById("time_restriction").value);
+    console.log(document.getElementById("views_restriction").value);
     let object = {
         "code": document.getElementById("code_snippet").value,
         "time": document.getElementById("time_restriction").value,
@@ -10,7 +12,12 @@ function send() {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", '/api/code/new', false)
     xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    xhr.send(json);
+
+    try {
+        xhr.send(json);
+    } catch (error) {
+        alert("Provide time restriction and view restriction values, for no restrictions enter 0.")
+    }
 
     if (xhr.status === 200) {
         alert("Success!");
